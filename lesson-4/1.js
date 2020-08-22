5ca2216fce507273063995d1d6c58954a5d7e443
Natalia Bilan <natalia_bilan@govitall.com>
2020-08-22 09:16:57 +0300

    added tasks from lesson-4

---
+++ lesson-4/1.js
@@ -0,0 +1,38 @@
+/**
+ * Задача 1.
+ *
+ * Создайте объект `person` c одним свойством `salary`.
+ * При чтении этого свойства должна возвращаться строка с текстом.
+ * Если до конца месяца осталось больше чем 20 дней — возвращается строка `good salary`, а если нет — `bad salary`
+ * 
+ * Условия:
+ * - Свойство salary обязательно должно быть геттером.
+ */
+
+const person = {
+    get salary() {
+        const today = new Date();
+        const dayOfTheCurrentMonth = today.getDate();
+        const currentYear = today.getFullYear();
+        const currentMonth = today.getMonth();
+        const days = new Date(currentYear, currentMonth + 1, 0);
+        const daysInCurrentMonth = days.getDate();
+
+        console.log(dayOfTheCurrentMonth);
+        console.log(currentMonth);
+        console.log(currentYear); 
+        console.log(`There\'re ${daysInCurrentMonth} days in current month!` );
+
+        if ((daysInCurrentMonth - dayOfTheCurrentMonth) > 20) {
+            return  console.log('good salary');
+        }
+
+        return  console.log('bad salary');   
+    }
+};
+
+// Решение
+
+person.salary; // good salary
+
+exports.person = person;
---
+++ lesson-4/2.js
@@ -0,0 +1,59 @@
+'use strict'
+/**
+ * Задача 2.
+ *
+ * Создайте объект `person` у которого будет 2 свойства: `rate` и `salary`.
+ *
+ * Свойство `rate` можно менять, но нельзя удалять.
+ * Также это свойство не должно участвовать в перечислении всех свойств при переборе.
+ *
+ * Свойство `salary` нельзя удалять.
+ * Также это свойство не должно участвовать в перечислении всех свойств при переборе.
+ * При чтении свойства `salary` возвращает результат умножения поля `rate` на текущее число в месяце.
+ *
+ * Если rate не установлен — возвращаем число 0.
+ * 
+ * Условия:
+ * - Свойство salary обязательно должно быть геттером.
+ */
+
+const person = {};
+
+Object.defineProperties(person, {
+    rate: {
+        value: null,
+        writable: true,
+        configurable: false,
+        enumerable: false,    
+    },
+
+    salary: {
+        enumerable: false,
+        configurable: false,
+
+        get () {
+            if(this.rate !== null) {
+                const today = new Date();
+                const dayOfTheCurrentMonth = today.getDate();
+                
+                return console.log(this.rate*dayOfTheCurrentMonth);
+            }
+
+            return console.log(0);  
+        }
+    }
+})
+
+// Решение
+console.log(person);
+
+person.rate = 30;
+// delete person.rate;
+
+
+// Предположим что сегодня 10 января, в этом случае это свойство возвращает число 300
+person.salary;
+// delete person.salary;
+
+
+exports.person = person;
