2a87057ef81032ce77c6e0b99fa2bb907e7e33ae
Natalia Bilan <natalia_bilan@govitall.com>
2020-09-21 15:05:26 +0300

    added tasks-10

---
+++ leson-10/1.js
@@ -0,0 +1,61 @@
+/**
+ * Задача 1.
+ *
+ * Напишите имплементацию функции Function.prototype.bind().
+ *
+ * Наша собственная функция bind() должна задавать контекст выполнения целевой функции,
+ * и возвращать новую, привязанную версию целевой функции.
+ *
+ * Функция bind() должна обладать следующими параметрами:
+ * - Первый параметр — это функция, контекст выполнения которой мы хотим привязать;
+ * - Второй параметр — это объект (не массив), в контексте которого нужно вызывать функцию из первого параметра;
+ * - Третий и все остальные параметры — это аргументы для вызова функции из первого параметра.
+ *
+ * Генерировать ошибки если:
+ * - Первый параметр не является типом function;
+ * - Второй параметр не является типом object;
+ * - Второй параметр является массивом.
+ *
+ * Условия:
+ * - Использовать встроенную функцию Function.prototype.bind() запрещено.
+ */
+
+// Решение
+const checkFunction = function (fn) {
+    if (typeof fn !== 'function') {
+        throw new Error('It should be a function!');
+    }
+}
+
+const checkContext = function (context) {
+    if (typeof context !== 'object' || Array.isArray(context)) {
+        throw new Error('It should be a function!');
+    } 
+}
+
+const bind = function (func, obj) {
+    checkFunction(func);
+    checkContext(obj);
+
+    const restArgs = [].slice.call(arguments, 2);
+
+    return function () {
+        const functionArgs = [].slice.call(arguments);
+
+        return func.apply(obj, restArgs.concat(functionArgs));
+    }    
+}
+
+function getName(greeting, message) {
+    return `${greeting} ${message} ${this.name}.`;
+}
+
+const user = { name: 'Walter', getName };
+const oliver = { name: 'Oliver' };
+
+const boundedGetName = bind(getName, oliver, 'Hello!', 'I am');
+
+console.log(user.getName('Hello!', 'My name is')); // Hello! My name is Walter.
+console.log(boundedGetName()); // Hello! I am Oliver.
+
+// exports.bind = bind;
---
+++ leson-10/2.js
@@ -0,0 +1,59 @@
+/**
+ * Задача 2.
+ *
+ * Напишите функцию calculate(), которая в качестве аргументов принимает неограниченное количество функций.
+ *
+ * При запуске calculate() последовательно запускает коллбек-функции из аргументов.
+ * Каждая коллбек-функция из цепочки в качестве своего аргумента принимает то, что возвращает предыдущая коллбек-функция.
+ * То есть возвращаемое значение каждой коллбек-функции из цепочки
+ * становится доступным из параметра следующей коллбек-функции в цепочке.
+ *
+ * Первая коллбек-функция не принимает параметров.
+ *
+ * Функция calculate() должна вернуть результат выполнения последней коллбек-функции из цепочки.
+ *
+ * Генерировать ошибки если:
+ * - Любой из аргументов функции calculate() не является функцией;
+ * - Любая функция из аргументов не вернула значение.
+ */
+
+// Решение
+
+const checkFunction = function (arrayOfCallbacks) {
+    const validArgs = arrayOfCallbacks.every(item => typeof item === 'function');
+
+    if (!validArgs) {
+        throw new Error('Every argument should be a function!');
+    }
+}
+
+const calculate = function (...cbs) {
+    checkFunction(cbs);
+
+    let result = cbs[0]();
+
+    for(let i = 1; i < cbs.length; i++){
+      if (!result) {
+            throw new Error('No result!');
+      }
+      result = cbs[i](result); 
+    }
+    
+    return result;
+}
+
+const result = calculate(
+    () => {
+        return 7;
+    },
+    prevResult => {
+        return prevResult + 4;
+    },
+    prevResult => {
+        return prevResult * 5;
+    },
+);
+
+console.log(result); // 55
+
+exports.calculate = calculate;
