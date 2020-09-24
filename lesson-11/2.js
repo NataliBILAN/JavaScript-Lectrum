62b7d73a3679b37297baaeca99929ed3df2801c9
Natalia Bilan <natalia_bilan@govitall.com>
2020-09-24 20:55:55 +0300

    added tasks(11 lesson)

---
+++ lesson-11/2.js
@@ -0,0 +1,39 @@
+/**
+ * Задача 2.
+ *
+ * Создайте функцию createFibonacciGenerator(),
+ * которая вернёт ещё одну функцию,
+ * каждый вызов которой будет возвращать число из последовательности Фибоначчи.
+ *
+ * Условия:
+ * - Задачу нужно решить с помощью замыкания.
+ */
+
+// Решение
+
+const createFibonacciGenerator = function () {
+    let prev = 0;
+    let next = 1;
+
+    const createFibonacci = () => {        
+        for(let i = 0; i < 1; i++){
+          next = prev + next;
+          prev = next - prev;
+        }
+        return prev;
+      }
+
+      return createFibonacci;
+}
+
+const generateFibonacciNumber = createFibonacciGenerator();
+
+console.log(generateFibonacciNumber()); // 1
+console.log(generateFibonacciNumber()); // 1
+console.log(generateFibonacciNumber()); // 2
+console.log(generateFibonacciNumber()); // 3
+console.log(generateFibonacciNumber()); // 5
+console.log(generateFibonacciNumber()); // 8
+console.log(generateFibonacciNumber()); // 13
+
+exports.createFibonacciGenerator = createFibonacciGenerator;
